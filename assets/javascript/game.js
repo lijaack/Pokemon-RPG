bf253dab84b75ccf9dab77bb7c85fc2e1768555e
Jack <lijaack@users.noreply.github.com>
2018-11-10 22:38:46 -0800

    added functions to move images and check for children

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,35 @@
 $( document ).ready(function() {
-
 });
+
+var pikachuHP = 100;
+$("#pikachu-hp").text(pikachuHP);
+
+var charmanderHP = 100;
+$("#charmander-hp").text(charmanderHP);
+
+var bulbasaurHP = 100;
+$("#bulbasaur-hp").text(bulbasaurHP);
+
+var squirtleHP = 100;
+$("#squirtle-hp").text(squirtleHP);
 
 function pikachu(){
     console.log("hi");
 }
 
 $("#pikachu").click(function(){
-    var img= $(this).clone();
-    $(this).attr("style","display:none");
-    $("#newdiv").append(img.removeAttr("class","pokemonimg"));
+    $(this).removeClass("pokemonimg");
+    var img= $("#pikachu2").clone();
+    $("#pikachu2").attr("style","display:none");
+    $("#newdiv").append(img);
   });
+
+  $("#bulbasaur").click(function(){
+    $(this).removeClass("pokemonimg");
+    var img= $("#bulbasaur2").clone();
+    $("#bulbasaur2").attr("style","display:none");
+    $("#newdiv3").append(img);
+  });
+
+  if ( $('#myfav').children().length > 0 ) {
+}
