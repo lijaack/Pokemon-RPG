f34a4a17d18ce5da2173b9827ee8f8238ce34185
Jack <lijaack@users.noreply.github.com>
2018-11-11 17:01:05 -0800

    pokemons move to proper positions on click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,81 @@
-$( document ).ready(function() {
-});
+$( document ).ready(newGame());
 
-var pikachuHP = 100;
-$("#pikachu-hp").text(pikachuHP);
 
-var charmanderHP = 100;
-$("#charmander-hp").text(charmanderHP);
+function newGame(){
+    var pikachuHP = 120;
+    $("#pikachu-hp").text(pikachuHP);
+    var charmanderHP = 150;
+    $("#charmander-hp").text(charmanderHP);
+    var bulbasaurHP = 140;
+    $("#bulbasaur-hp").text(bulbasaurHP);
+    var squirtleHP = 130;
+    $("#squirtle-hp").text(squirtleHP);
+    var yourPokemonHP = 0;
+    var opponentPokemonHP = 0;
+    var clonePokemon="";
 
-var bulbasaurHP = 100;
-$("#bulbasaur-hp").text(bulbasaurHP);
+    $("#nextMoves").text("choose your pokemon!");
 
-var squirtleHP = 100;
-$("#squirtle-hp").text(squirtleHP);
+    // if btn is clicked, check if battle ares are occupied, then move the pokemon
+    $(".btn").click(function(){
+        if ($("#yourPokemon").children().length===0){
+            console.log(this);         
+            $(this).removeClass("pokemonimg btn")
+            clonePokemon=$(this).closest("div").clone();
+            ($(this).parent("div")).attr("style", "display:none");
+            $("#yourPokemon").append(clonePokemon);    
+            $("#nextMoves").text("choose your opponent pokemon!");
 
-function pikachu(){
-    console.log("hi");
-}
+        }
+        else if($("#opponentPokemon").children().length===0){
+            $(this).removeClass("pokemonimg btn")
+            clonePokemon=$(this).closest("div").clone();
+            ($(this).parent("div")).attr("style", "display:none");
+            $("#opponentPokemon").append(clonePokemon);
+            $("#nextMoves").text("click attack to battle!");
 
-$("#pikachu").click(function(){
-    $(this).removeClass("pokemonimg");
-    var img= $("#pikachu2").clone();
-    $("#pikachu2").attr("style","display:none");
-    $("#newdiv").append(img);
-  });
+        }
+    });
+   
+    
+    
+    
+    
+    // check if "yourpokemon" or "opponentpokemon" area is occupied 
+    // function movePokemon(){
+    //     if ($("#yourPokemon").children().length===0){   
+    //         $("originalPokemon").attr("style","display:none");
+    //         $("#yourPokemon").append(clonePokemon);
 
-  $("#bulbasaur").click(function(){
-    $(this).removeClass("pokemonimg");
-    var img= $("#bulbasaur2").clone();
-    $("#bulbasaur2").attr("style","display:none");
-    $("#newdiv3").append(img);
-  });
+    //         moveToBattle();
+    //     }
+    //     else if($("#opponentPokemon").children.length===1){
+    //         console.log("i am 2");
+    //     }
+    // }
 
-  if ( $('#myfav').children().length > 0 ) {
+    // function moveToBattle(){
+    //     // console.log(mom);
+    //     // $(this).removeClass("btn");
+    //     // var clickedPokemon =$(this).clone();
+    // }
+
+
+
+    // $("#pokemon1").click(function(){
+    //     $(this).removeClass("pokemonimg");
+    //     var img= $("#pokemon1").clone();
+    //     $("#pokemon1").attr("style","display:none");
+    //     $("#yourPokemon").append(img);
+    // });
+
+    // $("#bulbasaur").click(function(){
+    //     $(this).removeClass("pokemonimg");
+    //     var img= $("#bulbasaur2").clone();
+    //     $("#bulbasaur2").attr("style","display:none");
+    //     $("#newdiv3").append(img);
+    // });
+
+    // if ( $('#myfav').children().length > 0 ) {
+    // }
 }
